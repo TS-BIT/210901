ccddaeb2e53849be7634b95c40aa56716e6110dd
TS BIT <tomassag@gmail.com>
2021-09-06 09:00:29 +0300

    Last changes 0903rea

--- js/i.js
+++ js/i.js
@@ -2,19 +2,33 @@
 
 const createDiv = () => document.createElement("DIV");
 
+
 const createDivText = (text) => {
     const div = document.createElement('DIV');
     const textNode = document.createTextNode(text);
     element.appendChild(texNode);
     return element;
 }
+*/
 
 const data = ['Labas', 'pats', 'tu labas'];
 console.log(data);
-const sorting =  data.sort( function(a,b) {
+const sorting =  data.sort( function(a, b) {
     return a.charAt(2).toLowerCase().charCodeAt() - b.charAt(2).toLowerCase().charCodeAt()
   });
 console.log(data);
+/*
+function compare(a, b) {
+    if (a is less than b by some ordering criterion) {
+      return -1;
+    }
+    if (a is greater than b by the ordering criterion) {
+      return 1;
+    }
+    // a must be equal to b
+    return 0;
+  }
+*/
 
 
 
@@ -32,14 +46,14 @@ const data4 = {
     trys: 'tu labas'
 }
 
-
+/*
 const createDivText = (text) => {
     const div = document.createElement('DIV');
     const textNode = data[i];
     element.appendChild(texNode);
     return element;
 }
-
+*/
 
 
 
@@ -55,6 +69,12 @@ const createDiv = () => document.createElement("DIV3");
 
 
 /*
+function createDivOld() {
+  const div = document.createElement('DIV');
+  return div;
+}
+
+
 function createDivOld1() {
   const div = document.createElement('DIV');
   return div;
@@ -71,7 +91,7 @@ const createDivOld3 = () => document.createElement('DIV');
 // dvieju ar daugiau eiluciu  f-jo susikuria pacios this. zodeli
 // vienos eilutes f-j pasiskolina zodeli is auksciau
 */
-
+/*
 function constructorDOM(inputTag, content, outputTag) {
     const NODE_Element = document.createElement(inputTag);
     const NODE_Content = document.createTextNode(content);
@@ -80,11 +100,11 @@ function constructorDOM(inputTag, content, outputTag) {
 }
 
 constructorDOM("div", "this is component", "root");
+*/
 
 
 
-
-
+/*
 function addElement () {
     // create a new div element
     const newDiv = document.createElement("div");
@@ -99,4 +119,4 @@ function addElement () {
     const currentDiv = document.getElementById("div1");
     document.body.insertBefore(newDiv, currentDiv);
   }
-
+*/
