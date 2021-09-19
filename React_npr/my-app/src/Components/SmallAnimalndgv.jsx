ec84cef430c418c09f1e52e2323c5d34935eff5b
TS BIT <tomassag@gmail.com>
2021-09-19 08:38:54 +0300

    Last changes 0916reaproj

--- React_npr/my-app/src/Components/SmallAnimalndgv.jsx
+++ React_npr/my-app/src/Components/SmallAnimalndgv.jsx
@@ -2,10 +2,7 @@ function SmallAnimal(props) {
     return (<div className="small-circle" style={{
         backgroundColor: props.color,
         borderRadius: props.animal == 'cow' ? '50%' : '5px'    
-    }}>
-        <span>{props.color}</span>
-        <button className="input-button-small">Go Home</button>
-        </div>);
+    }}>{props.color}</div>);
 }
 
 export default SmallAnimal;
