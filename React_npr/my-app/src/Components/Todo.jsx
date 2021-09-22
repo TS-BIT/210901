603e523264ce18408b34b66e1f261eebc4c80b0f
TS BIT <tomassag@gmail.com>
2021-09-22 20:33:52 +0300

    Last changes 0922reaproj

---
+++ React_npr/my-app/src/Components/Todo.jsx
@@ -0,0 +1,18 @@
+import User from './User';
+
+
+function Todo(props) {
+
+    return (
+        <div className="todo">
+            <div className='id'>{props.data.id}</div>
+            <User id={props.data}>
+            <div className="title">{props.data.title}</div>
+            <div className="status" style={
+                {backgroundColor: props.data.completed === false ? 'red' : 'green'}
+                }></div>
+        </div>
+    );
+}
+
+export default Todo;
---
+++ React_npr/my-app/src/Components/User.jsx
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+
+function User(props) {
+
+    const [name, setName] = useState('');
+
+    useEffect(()=>{
+        
+
+    }
+    return (
+        <div className="user">{name}</div>
+    )
+
+}
+
+
+export default User;
