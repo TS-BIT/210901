6bb258b94a1bd4214d23053b018493a091e08095
TS BIT <tomassag@gmail.com>
2021-09-29 12:46:13 +0300

    Last changes 0929reaproj

---
+++ React_npr/my-app/src/Actions/index.js
@@ -0,0 +1,15 @@
+import { CHANGE_COLOR, CHANGE_FANCY_COLOR } from '../Constants/types';
+
+export function changeColor() {
+    return {
+        type: CHANGE_COLOR
+    }
+}
+
+export function changeFancyColor(fancyColor) {
+    return {
+        type: CHANGE_FANCY_COLOR,
+        payload: fancyColor
+    }
+
+}
