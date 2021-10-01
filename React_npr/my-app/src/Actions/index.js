dc11a577eeedc7c35a3c49d1fa87da37260033ac
TS BIT <tomassag@gmail.com>
2021-10-01 10:54:26 +0300

    Last changes 0930reaproj

--- React_npr/my-app/src/Actions/index.js
+++ React_npr/my-app/src/Actions/index.js
@@ -1,4 +1,4 @@
-import { CHANGE_COLOR, CHANGE_FANCY_COLOR } from '../Constants/types';
+import { DELETE_ANIMAL, ADD_ANIMAL, CHANGE_COLOR, CHANGE_FANCY_COLOR, GET_ALL_FROM_LS, EDIT_ANIMAL, CHANGE_FARM } from '../Constants/types';
 
 export function changeColor() {
     return {
@@ -11,5 +11,41 @@ export function changeFancyColor(fancyColor) {
         type: CHANGE_FANCY_COLOR,
         payload: fancyColor
     }
+}
 
+
+// FARM
+
+export function getAllAnimals() {
+    return {
+        type: GET_ALL_FROM_LS,
+    }
+}
+
+export function createAnimal(animal) {
+    return {
+        type: ADD_ANIMAL,
+        payload: animal
+    }
+}
+
+export function removeAnimal(id) {
+    return {
+        type: DELETE_ANIMAL,
+        payload: id
+    }
+}
+
+export function changeAnimal(animal) {
+    return {
+        type: EDIT_ANIMAL,
+        payload: animal
+    }
+}
+
+export function moveAnimal(id) {
+    return {
+        type: CHANGE_FARM,
+        payload: id
+    }
 }
---
+++ React_npr/my-app/src/Components/App0930.jsx
@@ -0,0 +1,13 @@
+import Red from "./Red";
+
+
+function App() {
+
+    return (
+        <>
+            <Red></Red>
+        </>
+    );
+}
+
+export default App;
